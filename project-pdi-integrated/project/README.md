d985c90f962404c37e0f4a22b68e02d908adaa7b
jreyneke147 <144314004+jreyneke147@users.noreply.github.com>
2026-04-09 08:37:58 +0200

    Delete project-pdi-integrated/project directory

--- project-pdi-integrated/project/README.md
+++
@@ -1,3 +0,0 @@
-# HortgroMandE
-
-[![Open in Bolt](https://bolt.new/static/open-in-bolt.svg)](https://bolt.new/~/sb1-inybbgtv)
--- project-pdi-integrated/project/src/components/ui/LoadingSpinner.tsx
+++
@@ -1,9 +0,0 @@
-import { Loader2 } from 'lucide-react';
-
-export default function LoadingSpinner({ className = '' }: { className?: string }) {
-  return (
-    <div className={`flex items-center justify-center py-12 ${className}`}>
-      <Loader2 className="w-6 h-6 animate-spin text-brand-600" />
-    </div>
-  );
-}
